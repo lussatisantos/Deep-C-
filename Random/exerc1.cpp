66147548eaf9bbb2dae1aed9f479c041933b8469
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2023-11-08 16:06:37 +0000

    fstreams

--- Random/exerc1.cpp
+++ Random/exerc1.cpp
@@ -13,6 +13,19 @@ int main() {
 	arquivo << "testando o arquivo txt" << "\n";
 	arquivo << "teste realizado com sucesso!" << "\n";
 	
+	arquivo.close();
+	
+	arquivo.open("senhas.txt", ios::in);
+	
+	if(arquivo.is_open()){
+		while(getline(arquivo, linha)){
+			cout << linha << endl;
+		}
+	} else {
+		cout << "O ficheiro nao foi possivel ser aberto";
+	}
+	arquivo.close();
+	
 	return 0;
 	
 }
